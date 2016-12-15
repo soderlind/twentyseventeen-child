d1aa3b2a6fa8dcf3fc865c76aa7e560b0f47ff4f
soderlind <per@soderlind.no>
2016-12-16 00:26:01 +0100

    initial release

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+// Best practices, load parent style from functions.php don't use @import in style.css
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
